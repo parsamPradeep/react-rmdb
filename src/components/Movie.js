0b5818667438d6c12554c1eeb7cb82727e1eef86
parsamPradeep <parzam77@gmail.com>
2021-08-20 12:32:03 +0530

    useMovieFecth logic added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import Grid from './Grid';
 import Spinner from './Spinner';
 import NoImage from '../images/no-image.svg';
+import { useMovieFetch } from '../hooks/useMovieFetch';
 
 const Movie = () => {
+  const { movieId } = useParams(movieId);
+  const { state: movie, loading, error } = useMovieFecth(movieId);
+  console.log(movie);
   return (
     <>
       <div> Movie </div>
