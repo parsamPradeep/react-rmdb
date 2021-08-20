668be11d7cb0e6202669b566f71afe1f0569167c
parsamPradeep <parzam77@gmail.com>
2021-08-20 11:56:22 +0530

    Movie file updated for clickable icon

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import Grid from './Grid';
+import Spinner from './Spinner';
+import NoImage from '../images/no-image.svg';
 
-const Movie = () => <div> Movie </div>;
+const Movie = () => {
+  return (
+    <>
+      <div> Movie </div>
+    </>
+  );
+};
 export default Movie;
