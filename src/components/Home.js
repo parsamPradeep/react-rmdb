15614c3b53e0ccda2333d474d064f8a2c0f82c23
parsamPradeep <parzam77@gmail.com>
2021-08-22 22:47:17 +0530

    Home.js modifed to class component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-
+import API from '../API';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -10,50 +10,91 @@ import NoImage from '../images/no-image.svg';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
 import Button from './Button';
-const Home = () => {
-  const {
-    state,
-    loading,
-    error,
-    searchTerm,
-    setSearchTerm,
-    setIsLoadingMore
-  } = useHomeFetch();
 
-  console.log(state);
-
-  return (
-    <>
-      {!searchTerm && state.results[0] ? (
-        <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${
-            state.results[0].backdrop_path
-          }`}
-          title={state.results[0].original_title}
-          text={state.results[0].overview}
-        />
-      ) : null}
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <Grid header={searchTerm ? 'Search results' : 'Popular Movies'}>
-        {state.results.map(movie => (
-          <Thumb
-            key={movie.id}
-            clickable
-            image={
-              movie.poster_path
-                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                : NoImage
-            }
-            movieId={movie.id}
-          />
-        ))}
-      </Grid>
-      {loading && <Spinner />}
-      {state.page < state.total_pages && !loading && (
-        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
-      )}
-    </>
-  );
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
 };
+
+class Home extends Component {
+  state = {
+    movies: initialState,
+    searchTerm: '',
+    loading: false,
+    isLoadingMore: false,
+    error: false
+  };
+  fetchMovies = async (page, searchTerm = '') => {
+    try {
+      this.setState({ error: false, loading: true });
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      this.setState(prev => ({
+        movies: {
+          ...movies,
+          results:
+            page > 1
+              ? [...prev.movies.results, ...movies.results]
+              : [...movies.results]
+        },
+        loading: false
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  handleSearch = searchTerm => {
+    this.setState({ movies: initialState, searchTerm }, () =>
+      this.fetchMovies(1, this.state.searchTerm)
+    );
+  };
+
+  handleMore = () => {
+    this.fetchMovies(this.state.movies.page + 1, this.state.searchTerm);
+  };
+
+  componentDidMount() {
+    this.fetchMovies(1);
+  }
+  render() {
+    const { searchTerm, movies, loading, error } = this.state;
+    return (
+      <>
+        {!searchTerm && movies.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${
+              movies.results[0].backdrop_path
+            }`}
+            title={movies.results[0].original_title}
+            text={movies.results[0].overview}
+          />
+        ) : null}
+        <SearchBar setSearchTerm={this.handleSearch} />
+        <Grid header={searchTerm ? 'Search results' : 'Popular Movies'}>
+          {movies.results.map(movie => (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+              }
+              movieId={movie.id}
+            />
+          ))}
+        </Grid>
+        {loading && <Spinner />}
+        {movies.page < movies.total_pages && !loading && (
+          <Button text="Load More" callback={() => this.handleMore(true)} />
+        )}
+      </>
+    );
+  }
+}
 
 export default Home;
